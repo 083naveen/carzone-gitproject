9f4eb319646c3ed27e6a949bed32e44236a6c57c
083naveen <084naveenk@gmail.com>
2021-06-18 11:25:44 +0530

    added Car app and feteched the data

--- carzone/urls.py
+++ carzone/urls.py
@@ -17,8 +17,9 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from django.conf import settings
+from django.conf import settings 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
+    path('cars/',include('cars.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
